af881dfb409457e29aa06920b098c9a209739c36
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-06-13 17:35:18 -0500

    made a few more updates to the addItemForm. realized the item data needs to be updated as well

--- components/addItemForm.js
+++ components/addItemForm.js
@@ -4,7 +4,7 @@ const addItemForm = async (obj = {}) => {
   let items = await getAllItems();
   let domString = `
   <h2 class="form-header">Items!</h2>
-  <form id="add-item-form">
+  <form id=${obj.firebaseKey ? 'update-item-form' : 'add-item-form'}>
   `;
 
   const itemSelected = () => {
@@ -27,8 +27,10 @@ const addItemForm = async (obj = {}) => {
         <input type="radio" id="add-new-item" value="add-new-item">
         <label for="add-new-item">Add New Item</label>
         <input type="text" id="new-item-name" style="visibility: hidden">
+        <label for="add-new-name" style="visibility: hidden">Add New Item</label>
         <input type="text" id="new-item-price" style="visibility: hidden">
-        <button type="submit" id="submit-new-item-btn">Add/Edit Item</button>
+        <label for="new-item-price" style="visibility: hidden">Add New Item</label>
+        <button type="submit" id=${obj.firebaseKey ? 'update-item-btn' : 'add-item-btn'}>Add/Edit Item</button>
     </form>
     `;
 };
---
+++ events/formEvents.js
@@ -0,0 +1,9 @@
+const formEvents = () => {
+    document.querySelector('#form-container').addEventListener('click', (e) => {
+        if(e.target.id.includes('add-item')) {
+            if(document.querySelector('#add-new-item').checked !== true) {
+                
+            }
+        }
+    })
+}
